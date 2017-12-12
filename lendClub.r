561e9da8a986f98daf20ef057e85a6584a85dd7c
sprotou <sprotoulis@ucdavis.edu>
2017-12-12 13:36:54 -0800

    Percentiles
    Created quantile / percentile columns for some variables. Could be useful to factorize and check how "large" or "small" loans behave.

--- lendClub.r
+++ lendClub.r
@@ -8,7 +8,67 @@ getwd()
 rawData = read.csv('data.csv')
 
 regexp = "[[:digit:]]+"
-test1 = str_extract(rawData$emp_length, regexp)
+test1 = str_extract(rawData$emp_length, regexp) #turn emp_length from string to numeric
 
 modData = rawData %>%
   mutate(our_emp_length = as.numeric(test1))
+
+# dat %>% 
+#   mutate(var = replace(var, var != "Candy", "Not Candy"))
+modData = modData %>%
+  mutate(our_emp_length = replace(our_emp_length, emp_length == '< 1 year', 0))
+
+modData$our_emp_length[is.na(modData$our_emp_length)] = 0
+
+
+#x[is.na(x)] <- 0
+
+#categorize by loan size, installment size
+ggplot(data = modData) + geom_histogram(mapping = aes(modData$installment), bins = 200)
+
+loanAmtQuants = quantile(modData$loan_amnt, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1), na.rm=T) #added 0.9
+instQuants = quantile(modData$installment, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1), na.rm=T) #added 0.9
+revolBalQuants = quantile(modData$revol_bal, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1), na.rm =T) 
+dtiQuants = quantile(modData$dti, probs = c(0, 0.25, 0.5, 0.75, 0.9, 1), na.rm = T) 
+
+
+# case_when(
+#   x %% 35 == 0 ~ "fizz buzz",
+#   x %% 5 == 0 ~ "fizz",
+#   x %% 7 == 0 ~ "buzz",
+#   TRUE ~ as.character(x)
+# )
+modData = modData %>%
+  mutate(loanAmtPerc = case_when(
+     
+    loan_amnt < loanAmtQuants[2] ~ 0.25,
+    loan_amnt < loanAmtQuants[3] ~ 0.5,
+    loan_amnt < loanAmtQuants[4] ~ 0.75,
+    loan_amnt < loanAmtQuants[5] ~ 0.9,
+    loan_amnt <= loanAmtQuants[6] ~ 1
+    
+    ),
+    loanInstPerc = case_when(
+      installment < instQuants[2] ~ 0.25,
+      installment < instQuants[3] ~ 0.5,
+      installment < instQuants[4] ~ 0.75,
+      installment < instQuants[5] ~ 0.9,
+      installment <= instQuants[6] ~ 1
+    ),
+    dtiPerc = case_when(
+      dti < dtiQuants[2] ~ 0.25,
+      dti < dtiQuants[3] ~ 0.5,
+      dti < dtiQuants[4] ~ 0.75,
+      dti < dtiQuants[5] ~ 0.9,
+      dti <= dtiQuants[6] ~ 1
+    ),
+    revolBalPerc = case_when(
+      revol_bal < revolBalQuants[2] ~ 0.25,
+      revol_bal < revolBalQuants[3] ~ 0.5,
+      revol_bal < revolBalQuants[4] ~ 0.75,
+      revol_bal < revolBalQuants[5] ~ 0.9,
+      revol_bal <= revolBalQuants[6] ~ 1
+    )
+  )
+
+
